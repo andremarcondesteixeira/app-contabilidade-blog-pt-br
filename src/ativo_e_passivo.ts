553aa84d13541a98942959b5000a82d0f03679a8
André Teixeira <andremarcondesteixeira@gmail.com>
2022-09-03 23:22:01 -0300

    tidy

--- src/ativo_e_passivo.ts
+++ src/ativo_e_passivo.ts
@@ -1,5 +1,4 @@
 import type { Dinheiro } from "./dinheiro";
-import dinheiro from "./dinheiro";
 
 export type CoisaComValorMonetario = {
     nome: string;
@@ -29,49 +28,3 @@ export enum CirculanteFinanceiroOuOperacional {
     Financeiro,
     Operacional,
 }
-
-// exemplo
-const exemplosDeAtivos: Ativo[] = [
-    {
-        nome: "Dinheiro em caixa",
-        valor: dinheiro(5_000_00, "BRL"),
-        tangibilidade: Tangibilidade.Tangivel,
-        liquidez: Liquidez.Circulante,
-    },
-    {
-        nome: "Imóvel",
-        valor: dinheiro(500_000_00, "BRL"),
-        tangibilidade: Tangibilidade.Tangivel,
-        liquidez: Liquidez.NaoCirculante,
-    },
-    {
-        nome: "Saldo em conta bancária",
-        valor: dinheiro(50_000_00, "BRL"),
-        tangibilidade: Tangibilidade.Intangivel,
-        liquidez: Liquidez.Circulante,
-    },
-    {
-        nome: "Marga registrada",
-        valor: dinheiro(1_000_000_00, "BRL"),
-        tangibilidade: Tangibilidade.Intangivel,
-        liquidez: Liquidez.NaoCirculante,
-    },
-];
-
-const exemplosDePassivos: Passivo[] = [
-    {
-        nome: "Saldo negativo em conta bancária",
-        valor: dinheiro(1000_00, "BRL"),
-        liquidez: CirculanteFinanceiroOuOperacional.Financeiro,
-    },
-    {
-        nome: "Salários do mês",
-        valor: dinheiro(100_000_00, "BRL"),
-        liquidez: CirculanteFinanceiroOuOperacional.Operacional,
-    },
-    {
-        nome: "Empréstimo de longo prazo",
-        valor: dinheiro(200_000_00, "BRL"),
-        liquidez: Liquidez.NaoCirculante,
-    },
-];
