ea5486062f76f5d442598f0bed1d8357744dc078
André Teixeira <andremarcondesteixeira@gmail.com>
2022-09-03 21:36:21 -0300

    ativo e passivo

--- src/ativo_e_passivo.ts
+++ src/ativo_e_passivo.ts
@@ -1,10 +1,31 @@
 import type { Dinheiro } from "./dinheiro";
 
+export type Ativo = CoisaComValorMonetario & {
+    circulante_ou_nao_circulante: AtivoCirculanteOuNaoCirculante;
+};
+
+export type Passivo = CoisaComValorMonetario & {
+    circulante_ou_nao_circulante: PassivoCirculanteOuNaoCirculante;
+};
+
 export type CoisaComValorMonetario = {
     nome: string;
     valor: Dinheiro;
+    tangibilidade: Tangibilidade;
 };
 
-export type Ativo = CoisaComValorMonetario;
+export enum Tangibilidade {
+    Tangivel = "tangível",
+    Intangivel = "intangível",
+}
 
-export type Passivo = CoisaComValorMonetario;
+export enum AtivoCirculanteOuNaoCirculante {
+    Circulante = "ativo circulante",
+    NaoCirculante = "ativo não circulante",
+}
+
+export enum PassivoCirculanteOuNaoCirculante {
+    CirculanteOperacional = "passivo circulante operacional",
+    CirculanteFinanceiro = "passivo circulante financeiro",
+    NaoCirculante = "passivo não circulante",
+}
