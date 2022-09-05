ff126900c16d7292db479010a3e1ff4a1cb4584a
André Teixeira <andremarcondesteixeira@gmail.com>
2022-09-05 13:26:44 -0300

    wip

--- src/ativo_e_passivo.ts
+++ src/ativo_e_passivo.ts
@@ -1,18 +1,66 @@
 import type { Dinheiro } from "./dinheiro";
 
-export type CoisaComValorMonetario = {
-    nome: string;
-    valor: Dinheiro;
-};
+export abstract class CoisaComValorMonetario {
+    constructor(protected _nome: string, protected _valor: Dinheiro) {}
 
-export type Ativo = CoisaComValorMonetario & {
-    liquidez: Liquidez;
-    tangibilidade: Tangibilidade;
-};
+    get nome() {
+        return this._nome;
+    }
 
-export type Passivo = CoisaComValorMonetario & {
-    liquidez: CirculanteFinanceiroOuOperacional | Liquidez.NaoCirculante;
-};
+    get valor() {
+        return this._valor;
+    }
+}
+
+export class Ativo extends CoisaComValorMonetario {
+    private _liquidez: Liquidez;
+    private _tangibilidade: Tangibilidade;
+
+    constructor({
+        nome,
+        valor,
+        liquidez,
+        tangibilidade,
+    }: {
+        nome: string;
+        valor: Dinheiro;
+        liquidez: Liquidez;
+        tangibilidade: Tangibilidade;
+    }) {
+        super(nome, valor);
+        this._liquidez = liquidez;
+        this._tangibilidade = tangibilidade;
+    }
+
+    get liquidez() {
+        return this._liquidez;
+    }
+
+    get tangibilidade() {
+        return this._tangibilidade;
+    }
+}
+
+export class Passivo extends CoisaComValorMonetario {
+    private _liquidez: CirculanteFinanceiroOuOperacional | Liquidez.NaoCirculante;
+
+    constructor({
+        nome,
+        valor,
+        liquidez,
+    }: {
+        nome: string;
+        valor: Dinheiro;
+        liquidez: CirculanteFinanceiroOuOperacional | Liquidez.NaoCirculante;
+    }) {
+        super(nome, valor);
+        this._liquidez = liquidez;
+    }
+
+    get liquidez() {
+        return this._liquidez;
+    }
+}
 
 export enum Tangibilidade {
     Tangivel = "Tangível",
